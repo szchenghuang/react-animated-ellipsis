c3f651adae3458e788ec103e11d459601dacfd7a
szchenghuang <szchenghuang@gmail.com>
2017-04-22 16:49:09 +0800

    Reshape with Ecmascript, ES6 class, and prop-types.

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,10 @@
 //#############################################################################
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 require('animated-ellipsis');
 
@@ -14,49 +16,73 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 //#############################################################################
 // React components.
 //#############################################################################
-var ReactAnimatedEllipsis = _react2.default.createClass({
-    displayName: 'ReactAnimatedEllipsis',
-    propTypes: {
-        fontSize: _react2.default.PropTypes.string,
-        marginLeft: _react2.default.PropTypes.string,
-        spacing: _react2.default.PropTypes.string,
-        className: _react2.default.PropTypes.string,
-        style: _react2.default.PropTypes.object
-    },
-    getDefaultProps: function getDefaultProps() {
-        return {
-            fontSize: '2rem',
-            marginLeft: '0.1rem',
-            spacing: '0.1rem',
-            className: '',
-            style: {}
-        };
-    },
-    render: function render() {
-        var className = this.props.className;
-        var marginLeft = this.props.marginLeft;
-        var spacing = this.props.spacing;
-        if (this.props.fontSize) {
-            this.props.style.fontSize = this.props.fontSize;
-        }
-        var style = this.props.style;
+var ReactAnimatedEllipsis = function (_React$Component) {
+  _inherits(ReactAnimatedEllipsis, _React$Component);
 
-        return _react2.default.createElement('span', {
-            ref: this.animateEllipsis,
-            className: className,
-            style: style,
-            'data-margin-left': marginLeft,
-            'data-spacing': spacing });
-    },
-    animateEllipsis: function animateEllipsis(rootElem) {
-        rootElem && rootElem.animateEllipsis();
+  function ReactAnimatedEllipsis() {
+    _classCallCheck(this, ReactAnimatedEllipsis);
+
+    return _possibleConstructorReturn(this, (ReactAnimatedEllipsis.__proto__ || Object.getPrototypeOf(ReactAnimatedEllipsis)).apply(this, arguments));
+  }
+
+  _createClass(ReactAnimatedEllipsis, [{
+    key: 'render',
+    value: function render() {
+      var _props = this.props,
+          style = _props.style,
+          className = _props.className,
+          marginLeft = _props.marginLeft,
+          spacing = _props.spacing,
+          fontSize = _props.fontSize;
+
+      if (fontSize) {
+        style.fontSize = fontSize;
+      }
+
+      return _react2.default.createElement('span', {
+        ref: function ref(_ref) {
+          return _ref.animateEllipsis();
+        },
+        className: className,
+        style: style,
+        'data-margin-left': marginLeft,
+        'data-spacing': spacing });
     }
-});
+  }]);
+
+  return ReactAnimatedEllipsis;
+}(_react2.default.Component);
+
+ReactAnimatedEllipsis.propTypes = {
+  style: _react2.default.PropTypes.object,
+  className: _react2.default.PropTypes.string,
+  fontSize: _react2.default.PropTypes.string,
+  marginLeft: _react2.default.PropTypes.string,
+  spacing: _react2.default.PropTypes.string
+};
+
+ReactAnimatedEllipsis.defaultProps = {
+  style: {},
+  className: '',
+  fontSize: '2rem',
+  marginLeft: '0.1rem',
+  spacing: '0.1rem'
+};
 
 //#############################################################################
 // Exports.
--- src/index.js
+++ src/index.js
@@ -5,50 +5,44 @@
 //#############################################################################
 import 'animated-ellipsis';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //#############################################################################
 // React components.
 //#############################################################################
-const ReactAnimatedEllipsis = React.createClass({
-    displayName: 'ReactAnimatedEllipsis',
-    propTypes: {
-        fontSize: React.PropTypes.string,
-        marginLeft: React.PropTypes.string,
-        spacing: React.PropTypes.string,
-        className: React.PropTypes.string,
-        style: React.PropTypes.object
-    },
-    getDefaultProps() {
-        return {
-            fontSize: '2rem',
-            marginLeft: '0.1rem',
-            spacing: '0.1rem',
-            className: '',
-            style: {}
-        };
-    },
-    render() {
-        const className = this.props.className;
-        const marginLeft = this.props.marginLeft;
-        const spacing = this.props.spacing;
-        if ( this.props.fontSize ) {
-            this.props.style.fontSize = this.props.fontSize;
-        }
-        const style = this.props.style;
-
-        return (
-            <span
-                ref={ this.animateEllipsis }
-                className={ className }
-                style={ style }
-                data-margin-left={ marginLeft }
-                data-spacing={ spacing } />
-        );
-    },
-    animateEllipsis( rootElem ) {
-        rootElem && rootElem.animateEllipsis();
+class ReactAnimatedEllipsis extends React.Component {
+  render() {
+    const { style, className, marginLeft, spacing, fontSize } = this.props;
+    if ( fontSize ) {
+      style.fontSize = fontSize;
     }
-});
+
+    return (
+      <span
+        ref={ ref => ref.animateEllipsis() }
+        className={ className }
+        style={ style }
+        data-margin-left={ marginLeft }
+        data-spacing={ spacing } />
+    );
+  }
+}
+
+ReactAnimatedEllipsis.propTypes = {
+  style: React.PropTypes.object,
+  className: React.PropTypes.string,
+  fontSize: React.PropTypes.string,
+  marginLeft: React.PropTypes.string,
+  spacing: React.PropTypes.string
+};
+
+ReactAnimatedEllipsis.defaultProps = {
+  style: {},
+  className: '',
+  fontSize: '2rem',
+  marginLeft: '0.1rem',
+  spacing: '0.1rem'
+};
 
 //#############################################################################
 // Exports.
